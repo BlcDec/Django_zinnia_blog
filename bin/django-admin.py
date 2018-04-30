5caf750cfad22cd18a332311c05aceac069e565c
blcdec <1239176573@qq.com>
2018-04-30 02:21:13 -0700

    Initial commit

---
+++ bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/blcdec/ENV/bin/python3
+# EASY-INSTALL-SCRIPT: 'Django==2.0.4','django-admin.py'
+__requires__ = 'Django==2.0.4'
+__import__('pkg_resources').run_script('Django==2.0.4', 'django-admin.py')
